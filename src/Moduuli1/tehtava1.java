c4878d81431550265721ef4e4f570c1f96d48aec
onboni <onni.luova@gmail.com>
2024-01-16 13:26:02 +0200

    Ensimmäinen ja toinen tehtävä

--- src/Moduuli1/tehtava1.java
+++ src/Moduuli1/tehtava1.java
@@ -2,16 +2,6 @@ package src.Moduuli1;
 
 public class tehtava1 {
     public static void main(String[] args) {
-        int rivi = 4;
-
-        for (int i = 0; i < rivi; i++) {
-            for (int j = rivi - i; j > 1; j--) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= i * 2; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        System.out.print("Hello World!");
     }
 }
