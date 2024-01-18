2f5f2a08ad3ff121026860b49a44996028d62a88
onboni <onni.luova@gmail.com>
2024-01-18 12:41:40 +0200

    Moduuli4 1 ja 2 tehty.

---
+++ src/Mooduli4/tehtava1.java
@@ -0,0 +1,19 @@
+package src.Mooduli4;
+import java.util.Scanner;
+public class tehtava1 {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        String[] firstNames = {"John", "Bob", "Alice"};
+        String[] lastNames = {"Smith", "Jones", "Williams"};
+
+        System.out.println("Kuinka monta nimeä haluat generoida?");
+
+        int nimiLuku = lukija.nextInt();
+
+        for (int i = nimiLuku; i > 0; i--) {
+            System.out.print(firstNames[(int) (Math.random() * firstNames.length)]);
+            System.out.print(" " + lastNames[(int) (Math.random() * lastNames.length)] + "\n");
+        }
+    }
+}
---
+++ src/Mooduli4/tehtava2.java
@@ -0,0 +1,29 @@
+package src.Mooduli4;
+import java.util.Scanner;
+
+public class tehtava2 {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        int size = lukija.nextInt();
+        int[] lukuArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            lukuArray[i] = lukija.nextInt();
+        }
+
+        lukija.close();
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int start = 0; start < lukuArray.length; start++) {
+            int sum = 0;
+            for (int end = start; end < lukuArray.length; end++) {
+                sum += lukuArray[end];
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        System.out.println("Suurin summa oli " + maxSum);
+    }
+}
