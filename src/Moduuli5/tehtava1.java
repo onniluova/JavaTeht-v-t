2359696f4cfe5b7eb20f2a5775385ae38488c6ce
onniluo <onni.luova@gmail.com>
2024-02-15 09:46:18 +0200

    Muutoksia kommentteihin

--- src/Moduuli5/tehtava1.java
+++ src/Moduuli5/tehtava1.java
@@ -45,5 +45,14 @@ public class tehtava1 {
 
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Printing Complete");
     }
 }
